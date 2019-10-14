374cd80e9a2b194d3bf7413602f0720203c2ad1e
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-10-14 15:29:50 +0300

    Fixing Comment

--- Source/PuzzlePlatforms/MenuSystem/MainMenu.h
+++ Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -14,19 +14,9 @@ class PUZZLEPLATFORMS_API UMainMenu : public UUserWidget
 {
 	GENERATED_BODY()
 
-	
-protected:
-
-		virtual bool Initialize();
-
-private:
-
 		UPROPERTY(meta = (BindWidget))
 		class UButton* Host;
 
 		UPROPERTY(meta = (BindWidget))
 		class UButton* Join;
-
-		UFUNCTION()
-		void HostServer();
 };
